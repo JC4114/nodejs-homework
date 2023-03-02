2c55958e6c8ad5109bbaee3ffdec8d6433e387d7
JC4114 <4114114jc@gmail.com>
2023-03-02 15:34:03 +0200

    done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -17,10 +17,16 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch(
-  "/",
+  "/subscription",
   authenticate,
   validateBody(schemas.updateSubscriptionSchema),
   ctrl.updateSubscription
 );
 
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatarURL"),
+  ctrl.updateAvatar
+);
 module.exports = router;
