9af24d8b004881a223f28c5a9d274219ec43a48e
JC4114 <4114114jc@gmail.com>
2023-03-05 20:54:46 +0200

    done

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ const mongoose = require("mongoose");
 const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set("strictQuery", true);
 
-mongoose.connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT);
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
     console.log("Database connection successful");
   })
   .catch((error) => {
